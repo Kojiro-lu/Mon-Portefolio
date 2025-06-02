d2cea855197510b3c4035d0d09890856d7b87503
Anthony <kojiro5656i@gmail.com>
2025-06-02 12:34:00 +0200

    Création composant Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer-container">
+        <div className="icon-container"></div>
+        <p className="text">© 2025 - Tous droits réservés.</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
