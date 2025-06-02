0f6827f19ef9c502e4be6f2ed066512f07ed07f0
Anthony <kojiro5656i@gmail.com>
2025-06-02 12:05:26 +0200

    Création section Skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import "./skills.css";
+
+const Skills = () => {
+  return (
+    <>
+      <section className="skills-container"></section>
+    </>
+  );
+};
+
+export default Skills;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import "../Home/Home.css";
 import Hero from "../../components/Hero/Hero";
 import About from "../../components/About/About";
+import Skills from "../../components/Skills/Skills";
 
 function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
