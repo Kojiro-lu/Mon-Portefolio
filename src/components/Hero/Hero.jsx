d59662227bd9525c6d35636302d56b5b62e3cb47
Anthony <kojiro5656i@gmail.com>
2025-06-02 13:09:36 +0200

    Mise en place de framer-motion pour un efft d'apparition du texte dans le composant Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,34 @@
 import "./Hero.css";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h2>Bonjour !</h2>
-        <p>Je suis Anthony Vaudeville, Intégrateur Web </p>
-        <strong> et voici mon Portefolio.</strong>
+        <motion.h1
+          className="hero-title"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          Bonjour !
+        </motion.h1>
+        <motion.p
+          className="hero-subtitle"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Je suis Anthony Vaudeville, Intégrateur Web.
+        </motion.p>
+        <motion.p
+          className="hero-description"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          Bienvenue sur mon portfolio.
+        </motion.p>
       </div>
     </section>
   );
