d4f67066b99a6aa3a71c8b1806ba1af444344c16
Anthony <kojiro5656i@gmail.com>
2025-06-02 12:53:03 +0200

    Finalisation du Footer avec utilisation de react-icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import "./Footer.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <>
       <footer className="footer-container">
-        <div className="icon-container"></div>
-        <p className="text">© 2025 - Tous droits réservés.</p>
+        <div className="icon-container">
+          <a
+            href="https://github.com/Kojiro-lu"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Lien vers GitHub"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://linkedin.com/in/anthony-v-91b35a358"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Lien vers LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+        <p className="text-footer">© 2025 - Tous droits réservés.</p>
       </footer>
     </>
   );
